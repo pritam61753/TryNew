554fe52a54c5a7c8bdbac5576e0a015f9b210368
Pritam Singh <pritamsingh61753@gmail.com>
2022-11-16 13:14:55 +0530

    version 1

---
+++ src/main/java/demoTcs/TitleofPage.java
@@ -0,0 +1,44 @@
+package demoTcs;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TitleofPage {
+	
+	WebDriver driver;
+	
+	@BeforeMethod	
+	public void setUp() {
+		System.setProperty("Webdriver.chrome.driver", "C:\\Users\\win8\\Downloads\\chromedriver_win32 (1).chromedriver.exe");
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("https://www.google.com/");
+		
+	}
+	
+	@Test	
+	public void googleTitleTest() {
+		
+		String title = driver.getTitle();
+		System.out.println(title);	
+		}
+	
+	@Test
+	public void googleLogoTest() {
+		boolean b = driver.findElement(By.className("K1zIA rS4se")).isDisplayed();
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	
+
+}
